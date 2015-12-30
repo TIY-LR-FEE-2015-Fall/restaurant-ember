c0ebdb9bd7b08ab97308179fd1968a0e9e105348
Ryan Tablada <ryan.tablada@gmail.com>
2015-12-30 09:32:38 -0600

    Publish routes for mirage order and orderItems

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -5,6 +5,20 @@ export default function() {
   this.get('/menuItems/:id');
   this.put('/menuItems/:id');
   this.delete('/menuItems/:id');
+
+  this.get('/orders');
+  this.post('/orders');
+
+  this.get('/orders/:id');
+  this.put('/orders/:id');
+  this.delete('/orders/:id');
+
+  this.get('/orderItems');
+  this.post('/orderItems');
+
+  this.get('/orderItems/:id');
+  this.put('/orderItems/:id');
+  this.delete('/orderItems/:id');
 }
 
 /*
